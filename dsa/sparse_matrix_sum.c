7b09b81daee5c5468d8256efe3c27a99b97e568f
Nitin Tony Paul <108007300+nitintonypaul@users.noreply.github.com>
2025-08-11 22:13:17 +0530

    added sparse matrix operations

--- dsa/sparse_matrix_sum.c
+++ dsa/sparse_matrix_sum.c
@@ -1,25 +1,140 @@
 #include <stdio.h>
+#include <stdlib.h> 
 
-struct term {
-	int row;
-	int col;
-	int val;
+
+#define MAX_TERMS 10
+
+
+typedef struct {
+    int row;
+    int col;
+    int value;
+} SparseElement;
+
+
+void createSparseMatrix(SparseElement matrix[]) {
+    int numRows, numCols, numNonZero;
+
+    printf("Enter total number of rows: ");
+    scanf("%d", &numRows);
+
+    printf("Enter total number of columns: ");
+    scanf("%d", &numCols);
+
+    printf("Enter total number of non-zero elements: ");
+    scanf("%d", &numNonZero);
+
+    if (numNonZero > MAX_TERMS) {
+        printf("Number of non-zero elements exceeds the maximum of %d.\n", MAX_TERMS);
+        matrix[0].value = 0; 
+        return;
+    }
+
+    
+    matrix[0].row = numRows;
+    matrix[0].col = numCols;
+    matrix[0].value = numNonZero;
+
+    printf("Enter the non-zero elements (row, column, value) in sorted order:\n");
+    
+    for (int i = 1; i <= numNonZero; i++) {
+        scanf("%d %d %d", &matrix[i].row, &matrix[i].col, &matrix[i].value);
+    }
 }
 
-void main() {
 
-	int row1;
-	int col1;
-	printf("Enter the number of rows and columns for matix 1:\n");
-	scanf("%d%d", &row1, &col1);
-	
-	int row2;
-	int col2;
-	printf("Enter the number of rows and columns for matix 2:\n");
-	scanf("%d%d", &row2, &col2);
-	
-	i
+void displaySparseMatrix(const SparseElement matrix[]) {
+    int numNonZero = matrix[0].value;
+    if (matrix[0].row <= 0 || matrix[0].col <= 0) return;
+
+    for (int i = 1; i <= numNonZero; i++) {
+        printf("%d\t%d\t%d\n", matrix[i].row, matrix[i].col, matrix[i].value);
+    }
+}
 
 
+void addSparseMatrices(const SparseElement m1[], const SparseElement m2[], SparseElement result[]) {
+    
+    if (m1[0].row != m2[0].row || m1[0].col != m2[0].col) {
+        printf("\nMatrices cannot be added. Dimensions are different.\n");
+        result[0].row = 0; 
+        result[0].col = 0;
+        result[0].value = 0;
+        return;
+    }
 
+    
+    result[0].row = m1[0].row;
+    result[0].col = m1[0].col;
+    
+    int i = 1; 
+    int j = 1; 
+    int k = 1; 
+
+    while (i <= m1[0].value && j <= m2[0].value) {
+        if (m1[i].row < m2[j].row) {
+            result[k++] = m1[i++];
+        } 
+		else if (m1[i].row > m2[j].row) {
+            result[k++] = m2[j++];
+        } 
+		else { 
+            
+			if (m1[i].col < m2[j].col) {
+                result[k++] = m1[i++];
+            }
+
+			else if (m1[i].col > m2[j].col) {
+                result[k++] = m2[j++];
+            }
+
+			else { 
+                int sum = m1[i].value + m2[j].value;
+                if (sum != 0) {
+                    result[k].row = m1[i].row;
+                    result[k].col = m1[i].col;
+                    result[k].value = sum;
+                    k++;
+                }
+                i++;
+                j++;
+            }
+        }
+    }
+
+    while (i <= m1[0].value) {
+        result[k++] = m1[i++];
+    }
+
+    while (j <= m2[0].value) {
+        result[k++] = m2[j++];
+    }
+
+    result[0].value = k - 1;
+}
+
+int main() {
+    SparseElement matrix1[MAX_TERMS + 1];
+    SparseElement matrix2[MAX_TERMS + 1];
+    SparseElement sumMatrix[MAX_TERMS * 2 + 1]; 
+
+    printf("\nEnter details for the first sparse matrix:\n");
+    createSparseMatrix(matrix1);
+
+    printf("\nEnter details for the second sparse matrix:\n");
+    createSparseMatrix(matrix2);
+
+	printf("\nMatrix 1\n");
+    displaySparseMatrix(matrix1);
+	printf("\nMatrix 2\n");
+    displaySparseMatrix(matrix2);
+
+    addSparseMatrices(matrix1, matrix2, sumMatrix);
+
+    if (sumMatrix[0].row > 0) {
+        printf("\nSum");
+        displaySparseMatrix(sumMatrix);
+    }
+    
+    return 0;
 }
--- dsa/sparse_matrix_transpose.c
+++ dsa/sparse_matrix_transpose.c
@@ -1,14 +1,98 @@
 #include <stdio.h>
 
-struct term {
-	int row;
-	int col;
-	int val;
-};
-
-void main() {
-
-	
+#define MAX_TERMS 10
 
 
+typedef struct {
+    int row;
+    int col;
+    int value;
+} SparseElement;
+
+
+void createSparseMatrix(SparseElement matrix[]) {
+    int numRows, numCols, numNonZero;
+
+    printf("Enter total number of rows: ");
+    scanf("%d", &numRows);
+
+    printf("Enter total number of columns: ");
+    scanf("%d", &numCols);
+
+    printf("Enter total number of non-zero elements: ");
+    scanf("%d", &numNonZero);
+
+    if (numNonZero > MAX_TERMS) {
+        printf("Number of non-zero elements exceeds the maximum of %d.\n", MAX_TERMS);
+        matrix[0].value = 0; 
+        return;
+    }
+
+    
+    matrix[0].row = numRows;
+    matrix[0].col = numCols;
+    matrix[0].value = numNonZero;
+
+    printf("Enter the non-zero elements (row, column, value):\n");
+    
+    for (int i = 1; i <= numNonZero; i++) {
+        scanf("%d %d %d", &matrix[i].row, &matrix[i].col, &matrix[i].value);
+    }
+}
+
+
+void displaySparseMatrix(const SparseElement matrix[]) {
+    int numNonZero = matrix[0].value;
+    
+    for (int i = 1; i <= numNonZero; i++) {
+        printf("%d\t%d\t%d\n", matrix[i].row, matrix[i].col, matrix[i].value);
+    }
+}
+
+
+void transposeSparseMatrix(const SparseElement original[], SparseElement transpose[]) {
+    
+    int numRows = original[0].row;
+    int numCols = original[0].col;
+    int numTerms = original[0].value;
+
+    
+    transpose[0].row = numCols;    
+    transpose[0].col = numRows;    
+    transpose[0].value = numTerms; 
+
+    if (numTerms == 0) {
+        return; 
+    }
+
+    int current_pos = 1; 
+
+    
+    for (int c = 0; c < numCols; c++) {
+        
+        for (int i = 1; i <= numTerms; i++) {
+            
+            if (original[i].col == c) {
+                
+                transpose[current_pos].row = original[i].col;
+                transpose[current_pos].col = original[i].row;
+                transpose[current_pos].value = original[i].value;
+                current_pos++;
+            }
+        }
+    }
+}
+
+int main() {
+    
+    SparseElement original[MAX_TERMS + 1];
+    SparseElement transpose[MAX_TERMS + 1];
+
+    printf("Enter details for the original sparse matrix:\n");
+    createSparseMatrix(original);
+    displaySparseMatrix(original);
+    transposeSparseMatrix(original, transpose);
+    displaySparseMatrix(transpose);
+
+    return 0;
 }
